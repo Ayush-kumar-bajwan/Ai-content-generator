e59ad6f1432baf29f30dd4cd75006d25ece0b980
Ayush-kumar-bajwan <ayushbajwan@gmail.com>
2025-01-09 17:40:04 +0530

    changed model from 1.0 to 1.5

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./utils/schema.tsx",
   dbCredentials:{
-    url: "postgresql://ai-content_owner:j8rxJKPzt5XQ@ep-autumn-sea-a1e3yikv.ap-southeast-1.aws.neon.tech/ai-content?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
   }
  
 });
--- utils/AiModel.tsx
+++ utils/AiModel.tsx
@@ -8,13 +8,14 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.0-pro",
+    model: "gemini-1.5-flash-8b",
   });
   
   const generationConfig = {
-    temperature: 0.9,
-    topP: 1,
-    maxOutputTokens: 2048,
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
     responseMimeType: "text/plain",
   };
   
